95d000f620848397852999f0771932bbf21cb496
manote101 <manote.usa@gmail.com>
2021-06-06 14:51:48 +0000

    ok

---
+++ 01-Pod/commands.sh
@@ -0,0 +1,35 @@
+# check your current context
+kubectl config get-contexts                  # display list of contexts
+
+# permanently save the namespace for all subsequent kubectl commands in that context.
+# replace XX with your number
+kubectl config set-context --current --namespace=mylabXX    
+
+# create pod with CLI 
+kubectl run mynginx --image=nginx
+kubectl get pod -o wide
+kubectl describe pod mynginx
+
+# create pod with manifest file (Infrastruct as Code)
+kubectl apply -f single-pod.yaml
+
+# check pod IP address
+kubectl get pod -o wide
+
+
+# create second pod with CLI 
+kubectl apply -f pod2.yaml
+
+# check pod IP address
+kubectl get pod -o wide
+
+# notice that myapp-pod was re-created
+
+kubectl logs myapp-pod
+
+
+
+
+
+# when you finish, run cleanup
+kubectl delete pod --all
