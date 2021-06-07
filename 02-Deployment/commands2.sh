3c4854808313387f314cde86cea1643660a1de30
manote101 <manote.usa@gmail.com>
2021-06-07 08:47:08 +0000

    add command to check log

--- 02-Deployment/commands2.sh
+++ 02-Deployment/commands2.sh
@@ -1,5 +1,17 @@
 # use deployment & service to deploy web application
+# this will be called Main terminal
 
 kubectl apply -f myweb-deploy.yaml
 kubectl apply -f myweb-service.yaml
 kubectl get deploy,svc myweb
+
+# see pod
+kubectl get pod
+
+# open 3 terminals, each one run command to monitor each pod
+kubectl logs <pod-name-1> -f      # run on terminal #1
+kubectl logs <pod-name-2> -f      # run on terminal #2
+kubectl logs <pod-name-3> -f      # run on terminal #3
+
+# on main terminal, run curl 3-4 time and see log status
+curl <ip-address-of-service>
