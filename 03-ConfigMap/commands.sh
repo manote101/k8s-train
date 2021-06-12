766b90d8cea7c5c1d78a67bdbcf682a4218fc55f
manote101 <manote.usa@gmail.com>
2021-06-12 14:38:06 +0000

    new

---
+++ 03-ConfigMap/commands.sh
@@ -0,0 +1,28 @@
+# create ConfigMap & Secret
+
+kubectl apply -f config-file.yaml
+kubectl get configmap 
+kubectl describe cm homepage-file
+
+kubectl apply -f secret.yaml
+kubectl get secret 
+kubectl describe mysecret-file
+
+# see content of deploy-no-config.yaml
+more deploy-no-config.yaml
+
+# firstly, see default nginx page.
+kubectl apply -f deploy-no-config.yaml
+kubectl get pod
+kubect exec <pod-name> -- curl localhost
+
+# this time, we will update index.html using ConfigMap
+kubectl apply -f deploy-with-configmap.yaml
+kubectl get pod
+kubectl exec <pod-name> -- curl localhost
+kubectl exec <pod-name> -- ls -l /usr/share/nginx/html
+kubectl exec <pod-name> -- cat /usr/share/nginx/html/index.html
+
+# see secret.file which we created with ConfigMap
+kubectl exec <pod-name> -- ls /usr/share/nginx/html/secret
+kubectl exec <pod-name> -- cat /usr/share/nginx/html/secret/secret.file
