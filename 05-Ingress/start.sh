edc93237d8b02303dfeef4d7e23e7413b4f2e79b
manote101 <manote.usa@gmail.com>
2021-11-14 11:57:44 +0700

    Update start.sh

--- 05-Ingress/start.sh
+++ 05-Ingress/start.sh
@@ -1,7 +1,11 @@
+```ShellSession
 kubectl apply -f web-deploy.yaml
 kubectl apply -f web-service.yaml
 
 kubectl apply -f shopping-config.yaml
 kubectl apply -f shopping-deploy.yaml
 kubectl apply -f shopping-service.yaml
+
+kubectl apply -f echo-server.yaml
 kubectl apply -f ingress.yaml
+```
