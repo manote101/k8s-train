9d79b66ed1971eee3a6f051413e82923e03fa036
manote101 <manote.usa@gmail.com>
2021-11-14 11:58:44 +0700

    Update stop.sh

--- 05-Ingress/stop.sh
+++ 05-Ingress/stop.sh
@@ -1,7 +1,11 @@
+```ShellSession
 kubectl delete -f web-deploy.yaml
 kubectl delete -f web-service.yaml
 
 kubectl delete -f shopping-config.yaml
 kubectl delete -f shopping-deploy.yaml
 kubectl delete -f shopping-service.yaml
+
+kubectl delete -f echo-server.yaml
 kubectl delete -f ingress.yaml
+```
